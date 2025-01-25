7f9c7bcebbac03d978b4d46fdc94acbc20c78db0
sahal2255 <sahalvv9656@gmail.com>
2025-01-25 11:37:23 +0530

    env

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
+// The free tier of Vercel does not support environment variables (.env), which may cause issues with configuration and deployment.
 const axiosInstance = axios.create({
   baseURL:'https://product-alpha-gray.vercel.app'||'http://localhost:3005', 
   withCredentials: true,
 });
 
 export default axiosInstance;
-
-// The free tier of Vercel does not support environment variables (.env), which may cause issues with configuration and deployment.
